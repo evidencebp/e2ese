3700bd0d960552b26de04a9e3733de247d282928
idanacumen <idan@acumen.io>
2021-07-28 13:30:03 +0300

    Created the main query file

---
+++ src/queries/main.sql
@@ -0,0 +1,6 @@
+# This scripts document the actions needed to build the data set
+
+# Build the infrastructure using https://github.com/evidencebp/general
+
+# Run repo_split.sql
+# splits repos to train/validation/test and add repo license
