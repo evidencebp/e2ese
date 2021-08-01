f2e5ef8291f96db8e070071b2c8c10aff6f2f88f
idanacumen <idan@acumen.io>
2021-08-01 16:29:16 +0300

    Reuse split utils

--- src/queries/repo_split.sql
+++ src/queries/repo_split.sql
@@ -1,35 +1,6 @@
-CREATE OR REPLACE FUNCTION
-general.bq_repo_type
- (repo_name string)
- RETURNS string
-AS (
-    case
-        when substr(TO_HEX(md5(repo_name)), 1,1) = '0'  then 'Validation'
-        when substr(TO_HEX(md5(repo_name)), 1,1) = '1'  then 'Test'
-        else 'Train' end # Train
 
- )
-;
-
-
-WITH tab AS (
-  SELECT  'tensorflow/tensorflow' AS repo_name
-            , 'Train' as expected
-    UNION ALL SELECT '3846masa/upload-gphotos'
-                    , 'Validation'
-
-    UNION ALL SELECT '3scale/echo-api'
-                    , 'Test'
-
-    UNION ALL SELECT null
-                    , null
-)
-SELECT repo_name
-, expected
-, general.bq_repo_type(repo_name) as actual
-, general.bq_repo_type(repo_name) = expected as pass
-FROM tab as testing
-;
+# general.bq_repo_type is defined in split_util.sql in
+# https://github.com/evidencebp/general
 
 drop table if exists general.repos_split;
 
@@ -56,7 +27,7 @@ r.*
 , 'mpl-2.0'
 , 'unlicense'
 ) as oss_license_found
-, general.bq_repo_type(r.repo_name) as type
+, general.bq_repo_split(r.repo_name) as type
 from
 general.repos as r
 left join
