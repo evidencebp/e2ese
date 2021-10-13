53a3364c55b9209204e49521bd19776f281ceb82
idanacumen <idan@acumen.io>
2021-10-13 10:44:24 +0300

    October

--- src/scripts/compress_and_extract.sh
+++ src/scripts/compress_and_extract.sh
@@ -1,9 +1,9 @@
 # Compres test/validation/train files (in test/validation/train directories)
-tar cvzf - test | split -b 40m - june_2021_java_file_content_test.csv.tar.gz
-tar cvzf - validation | split -b 40m - june_2021_java_file_content_validation.csv.tar.gz
-tar cvzf - train | split -b 40m - june_2021_java_file_content_train.csv.tar.gz
+tar cvzf - train | split -b 40m - october_2021_python_file_content_train.csv.tar.gz
+tar cvzf - validation | split -b 40m - october_2021_python_file_content_validation.csv.tar.gz
+tar cvzf - test | split -b 40m - october_2021_python_file_content_test.csv.tar.gz
 
 # Extract test/validation/train files
-cat june_2021_java_file_content_test.csv.tar.gz.* | tar xzvf -
-cat june_2021_java_file_content_validation.csv.tar.gz.* | tar xzvf -
-cat june_2021_java_file_content_train.csv.tar.gz.* | tar xzvf -
+cat october_2021_python_file_content_train.csv.tar.gz.* | tar xzvf -
+cat october_2021_python_file_content_validation.csv.tar.gz.* | tar xzvf -
+cat october_2021_python_file_content_test.csv.tar.gz.* | tar xzvf -
