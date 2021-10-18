924c7a6caad6a804530a9520ab7b92ce2c59a962
idanacumen <idan@acumen.io>
2021-10-18 14:51:35 +0300

    Use relevant commits for extraction

--- src/queries/generate_commits_dataset.sql
+++ src/queries/generate_commits_dataset.sql
@@ -1,57 +1,30 @@
-drop table if exists general.june_aug_2021_java_commits;
+drop table if exists general.relevant_enhanced_commits_extraction;
 
 create table
-general.june_aug_2021_java_commits
+general.relevant_enhanced_commits_extraction
 as
 select
-r.repo_name
+ec.repo_name
 , ec.commit
 , ec.message
-, general.bq_repo_split(r.repo_name) as repo_split
+, general.bq_repo_split(ec.repo_name) as repo_split
 , general.bq_commit_split(commit) as commit_split
 from
-general.enhanced_commits as ec
-join
-general.repos_split as r
-on
-ec.repo_name = r.repo_name
-where
-r.language = 'Java'
-and
-oss_license_found
-and
-# END_DATE
-(PARSE_DATE('%d/%m/%Y',  '1/8/2021') > date(ec.commit_timestamp))
-and
-# START_DATE
-(PARSE_DATE('%d/%m/%Y',  '1/6/2021') <= date(ec.commit_timestamp))
+general.relevant_enhanced_commits as ec
+
 ;
 
-drop table if exists general.june_aug_2021_java_commits_files;
+drop table if exists general.relevant_commits_files_extraction;
 
 create table
-general.june_aug_2021_java_commits_files
+general.relevant_commits_files_extraction
 as
 select
-r.repo_name
+cf.repo_name
 , cf.commit
 , cf.file
-, general.bq_repo_split(r.repo_name) as repo_split
+, general.bq_repo_split(cf.repo_name) as repo_split
 , general.bq_commit_split(commit) as commit_split
 from
-general.commits_files as cf
-join
-general.repos_split as r
-on
-cf.repo_name = r.repo_name
-where
-r.language = 'Java'
-and
-oss_license_found
-and
-# END_DATE
-(PARSE_DATE('%d/%m/%Y',  '1/8/2021') > date(cf.commit_timestamp))
-and
-# START_DATE
-(PARSE_DATE('%d/%m/%Y',  '1/6/2021') <= date(cf.commit_timestamp))
+general.relevant_commits_files as cf
 ;
