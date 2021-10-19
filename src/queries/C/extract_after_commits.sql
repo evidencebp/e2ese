180f951720502b3062127a9bd9f1d599a055de17
idanacumen <idan@acumen.io>
2021-10-19 15:08:30 +0300

    Added extract after commits

---
+++ src/queries/C/extract_after_commits.sql
@@ -0,0 +1,22 @@
+drop table if exists general.after_commits;
+
+create table
+general.after_commits
+as
+select
+*
+from
+general.relevant_enhanced_commits_extraction
+
+;
+
+drop table if exists general.after_commits_files;
+
+create table
+general.after_commits_files
+as
+select
+*
+from
+general.relevant_commits_files_extraction
+;
