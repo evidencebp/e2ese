fe44092c7878beaac7999670b057dfaf8c09c470
idanacumen <idan@acumen.io>
2021-10-21 14:56:40 +0300

    Added file properties compression

--- src/scripts/compress_and_extract.sh
+++ src/scripts/compress_and_extract.sh
@@ -3,7 +3,13 @@ tar cvzf - train | split -b 40m - october_2021_python_file_content_train.csv.tar
 tar cvzf - validation | split -b 40m - october_2021_python_file_content_validation.csv.tar.gz
 tar cvzf - test | split -b 40m - october_2021_python_file_content_test.csv.tar.gz
 
+tar cvzf - compress | split -b 40m - previous_file_properties.csv.tar.gz
+tar cvzf - compress | split -b 40m - after_file_properties.csv.tar.gz
+
 # Extract test/validation/train files
 cat october_2021_python_file_content_train.csv.tar.gz.* | tar xzvf -
 cat october_2021_python_file_content_validation.csv.tar.gz.* | tar xzvf -
 cat october_2021_python_file_content_test.csv.tar.gz.* | tar xzvf -
+
+tar previous_file_properties.csv.tar.gz.* | tar xzvf -
+tar after_file_properties.csv.tar.gz.* | tar xzvf -
