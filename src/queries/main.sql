1634ad14940ad407368704deef304275d27795bd
idanacumen <idan@acumen.io>
2021-10-25 16:14:51 +0300

    Change to use language specific

--- src/queries/main.sql
+++ src/queries/main.sql
@@ -2,19 +2,45 @@
 
 # Build the infrastructure using https://github.com/evidencebp/general
 
-# Run repo_split.sql
-# splits repos to train/validation/test and add repo license
+# Follow instructions using the specific language directory
+
+
+#### Per language
+# Modify and run prepare_source_extraction.sql
+
+# Run lang/prepare_source_extraction.sql
 
 # Run generate_dataset.sql
 
-# Run generate_code_similarity_dataset.sql
+# Run lang/prepare_before_extraction.sql
 
-# Run file_properties_per_period.sql
+# Run file_properties_on_relevant.sql
 
-# Run generate_labels.sql
+# Run commit_file_prev_touch.sql
 
-# Run program_repair.sql
+# Run extract_previous_file_properties.sql
+
+# Run lang/prepare_after_extraction.sql
+
+# Run file_properties_on_relevant.sql
+
+# Run commit_file_prev_touch.sql
+
+# Run lang/extract_after_file_properties.sql
 
 # Run generate_commits_dataset.sql
 
+# Run extract_after_commits.sql
+
+# Run generate_labels.sql
+
+
+
+# Run program_repair.sql
+
 # Run generate_difficulty_pairs.sql
+
+# Run lang/prepare_code_similarity.sql
+# Run generate_code_similarity_dataset.sql
+
+# Run cleanup.sql
