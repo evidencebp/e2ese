49c1a91c00be2d27070ffe047c871b62e3134589
idanacumen <idan@acumen.io>
2021-10-25 16:01:06 +0300

    reorg files

--- src/queries/generate_dataset.sql
+++ src/queries/generate_dataset.sql
@@ -1,33 +1,8 @@
-drop table if exists general.october_2021_java_file_content;
+
+drop table if exists general.lang_relevant_content_train;
 
 create table
-general.october_2021_java_file_content
-as
-select
-id
-, cnt.repo_name
-, path as file
-, content
-, general.bq_repo_split(cnt.repo_name) as repo_split
-, general.bq_file_split(cnt.repo_name, path) as file_split
-from
-general.contents_1_october_2021 as cnt
-join
-general.repos_split as r
-on
-cnt.repo_name = r.repo_name
-where
-r.language = 'Java'
-and
-extension = '.java'
-and
-oss_license_found
-;
-
-drop table if exists general.october_2021_java_file_content_train;
-
-create table
-general.october_2021_java_file_content_train
+general.lang_relevant_content_train
 as
 select
 id
@@ -36,15 +11,16 @@ id
 , content
 , general.bq_file_split(cnt.repo_name, file) as file_split
 from
-general.october_2021_java_file_content as cnt
+general.lang_relevant_content as cnt
 where
 general.bq_repo_split(cnt.repo_name) = 'Train'
 ;
 
-drop table if exists general.october_2021_java_file_content_test;
+
+drop table if exists general.lang_relevant_content_validation;
 
 create table
-general.october_2021_java_file_content_test
+general.lang_relevant_content_validation
 as
 select
 id
@@ -53,51 +29,24 @@ id
 , content
 , general.bq_file_split(cnt.repo_name, file) as file_split
 from
-general.october_2021_java_file_content as cnt
-where
-general.bq_repo_split(cnt.repo_name) = 'Test'
-;
-
-
-drop table if exists general.october_2021_java_file_content_validation;
-
-create table
-general.october_2021_java_file_content_validation
-as
-select
-id
-, cnt.repo_name
-, file
-, content
-, general.bq_file_split(cnt.repo_name, file) as file_split
-from
-general.october_2021_java_file_content as cnt
+general.lang_relevant_content as cnt
 where
 general.bq_repo_split(cnt.repo_name) = 'Validation'
 ;
 
-# TODO - properties prior to June
-# TODO - properties from october to August
-drop table if exists general.october_2020_java_file_properties;
+drop table if exists general.lang_relevant_content_test;
 
 create table
-general.october_2020_java_file_properties
+general.lang_relevant_content_test
 as
 select
 id
-, fp.*
-, general.bq_repo_split(cnt.repo_name) as repo_split
-, general.bq_file_split(cnt.repo_name, path) as file_split
+, cnt.repo_name
+, file
+, content
+, general.bq_file_split(cnt.repo_name, file) as file_split
 from
-general.october_2020_java_file_content as cnt
-join
-general.repos_split as r
-on
-cnt.repo_name = r.repo_name
-join
-general.file_properties as fp
-on
-cnt.repo_name = fp.repo_name
-and
-cnt.file = fp.file
+general.lang_relevant_content as cnt
+where
+general.bq_repo_split(cnt.repo_name) = 'Test'
 ;
