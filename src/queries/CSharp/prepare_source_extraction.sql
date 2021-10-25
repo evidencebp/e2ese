0a49c1aed4c9c16f0f248d6f5ce57f1461d53dd7
idanacumen <idan@acumen.io>
2021-10-25 16:53:13 +0300

    Added source extraction

---
+++ src/queries/CSharp/prepare_source_extraction.sql
@@ -0,0 +1,58 @@
+
+drop table if exists general.relevant_repos;
+
+
+create table
+general.relevant_repos
+as
+select
+repo_name
+from
+general.repos_split as r
+where
+# Note - set language when porting to a new language
+r.language = 'C#'
+and
+oss_license_found
+;
+
+drop view if exists general.relevant_content;
+
+create view
+general.relevant_content
+as
+select
+*
+from
+# Note - set proper content version
+general.contents_1_june_2021
+;
+
+
+
+drop table if exists general.lang_relevant_content;
+
+create table
+general.lang_relevant_content
+as
+select
+id
+, cnt.repo_name
+, path as file
+, content
+, general.bq_repo_split(cnt.repo_name) as repo_split
+, general.bq_file_split(cnt.repo_name, path) as file_split
+from
+general.relevant_content as cnt
+join
+general.repos_split as r
+on
+cnt.repo_name = r.repo_name
+where
+# Note - set language when porting to a new language
+r.language = 'C#'
+and
+extension = '.cs'
+and
+oss_license_found
+;
