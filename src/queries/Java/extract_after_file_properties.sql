3d6b53cda888aeaf82e70eff476db9e47688b044
idanacumen <idan@acumen.io>
2021-11-02 11:02:57 +0200

    Java & Python updates

---
+++ src/queries/Java/extract_after_file_properties.sql
@@ -0,0 +1,29 @@
+# Java/extract_after_file_properties.sql
+drop table if exists general.after_file_properties;
+
+create table
+general.after_file_properties
+as
+SELECT repo_name
+, file
+, is_test
+, commits
+, ccp
+, corrective_commits
+, refactor_mle
+, avg_coupling_code_size_cut
+, authors
+, same_day_duration_avg
+, prev_touch_ago
+, bug_prev_touch_ago
+, one_file_fix_rate
+, one_file_refactor_rate
+, textual_abstraction_commits_rate
+, abs_content_ratio
+, size
+, performance_rate
+, security_rate
+FROM general.relevant_file_properties
+where
+extension = '.java'
+;
